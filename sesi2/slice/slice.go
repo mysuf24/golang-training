13bfae26a33800fc2ce78dd77dbbab7e27403d58
mysuf24 <mochammadyusuf.1008@gmail.com>
2025-04-16 08:44:26 +0700

    slice done

--- sesi2/slice/slice.go
+++ sesi2/slice/slice.go
@@ -36,7 +36,60 @@ func main() {
 	var buah2 = []string{"anggur", "salak", "nanas"}
 
 	buah1 = append(buah1, buah2...)
+	// copy func
+	cp := copy(buah1, buah2)
+	fmt.Printf("%#v \n", buah1)
+	fmt.Printf("%#v \n", cp)
 
-	fmt.Printf("%#v", buah1)
+	//slicing
+	var lookism = []string{"joggun", "junggo", "yohan", "hyungseok"}
 
+	var lookism1 = lookism[1:4]
+	fmt.Printf("%#v\n", lookism1)
+
+	var lookism2 = lookism[3:]
+	fmt.Printf("%#v\n", lookism2)
+
+	var lookism3 = lookism[:3]
+	fmt.Printf("%#v\n", lookism3)
+
+	var lookism4 = lookism[:]
+	fmt.Printf("%#v\n", lookism4)
+
+	//slicing append
+	lookism = append(lookism[:3], "Zin")
+	fmt.Printf("%#v\n", lookism)
+
+	//backing
+	var lookismBacking = lookism[2:4]
+	lookismBacking[0] = "God Dog"
+	fmt.Printf("%#v\n", lookismBacking)
+
+	//cap func
+	//cap untuk mengetahui isi dari array maupun slice
+	//len untuk mengetahui panjang dari array maupun slice
+	var valorant = []string{"chyper", "waylay", "killjoy", "reyna"}
+	fmt.Println("Valorant cap:", cap(valorant)) //4
+	fmt.Println("Valorant len:", len(valorant)) //4
+	fmt.Println(strings.Repeat("#", 30))
+
+	var valorant1 = valorant[1:]
+	fmt.Println("Valorant cap:", cap(valorant1)) //3
+	fmt.Println("Valorant len:", len(valorant1)) //3
+	fmt.Println(strings.Repeat("#", 30))
+
+	var valorant2 = valorant[0:3]
+	fmt.Println("Valorant cap:", cap(valorant2)) //4
+	fmt.Println("Valorant len:", len(valorant2)) //3
+	fmt.Println(strings.Repeat("#", 30))
+
+	//newbacking
+	roko := []string{"malboro", "garpit", "surya", "mild"}
+	newRoko := []string{}
+
+	newRoko = append(newRoko, roko[0:2]...)
+
+	roko[0] = "esse"
+	fmt.Println("roko: ", roko)
+	fmt.Println("new roko: ", newRoko)
 }
